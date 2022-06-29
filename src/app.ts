c196b66e4568be2dc5f56fcc33ece480bbb1aea7
LeonPJ <leon19941214@gmail.com>
2022-06-29 19:42:12 +0800

    add attribute in env

--- src/app.ts
+++ src/app.ts
@@ -10,13 +10,15 @@ import { router as authRoute } from './routes/auth';
 import { router as orderRoute } from './routes/order';
 
 interface ENV {
-    DB_CONNECT: string | undefined;
-    TOKEN_SECRET: string | undefined;
+    DB_CONNECT: string | object;
+    TOKEN_SECRET: string | object;
 }
 
 // connect db
 // mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('connect to db'));
+// mongoose.connect(process.env.DB_CONNECT, () => console.log('connect to db'));
 mongoose.connect(process.env.DB_CONNECT, () => console.log('connect to db'));
+
 
 
 // middleware
